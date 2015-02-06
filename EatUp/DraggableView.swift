04c0a07cec35129f963f68641ff1a9d719a93498
Jeffro Ligon <jtligon@gmail.com>
2015-02-06 09:24:30 -0500

    sorted out draggableView and OverlayView

--- EatUp/DraggableView.swift
+++ EatUp/DraggableView.swift
@@ -1,5 +1,5 @@
 //
-//  DraggableView.swift
+//  OverlayView.swift
 //  EatUp
 //
 //  Created by Jeff Ligon on 2/5/15.
@@ -8,55 +8,18 @@
 
 import UIKit
 
-
-
-class DraggableView: UIView {
-    enum OverLayMode: Printable{
-        case OverlayModeLeft
-        case OverlayModeRight
-        
-        var description: String{
-        switch self{
-        case .OverlayModeRight: return "Right"
-        case .OverlayModeLeft: return "Left"
-            }
-        }
-    }
-    var mode:OverLayMode!
-    var imageView:UIImageView!
-    
-    override init(frame: CGRect){
+class DraggableView : UIView{
+    override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.mode = OverLayMode.OverlayModeLeft
-        
-        backgroundColor = UIColor.whiteColor()
-        var thumbDownImage:UIImage = UIImage(named: "ThumbDown")!
-        imageView = UIImageView(image: thumbDownImage)
-        addSubview(imageView)
-        
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
 }
 
 extension DraggableView{
-    func setMode(mode: OverLayMode)->(){
-        if self.mode == mode{
-            return
-        }
-        if mode == OverLayMode.OverlayModeLeft{
-            imageView.image = UIImage(named: "ThumbDown")
-        }else{
-            imageView.image = UIImage(named: "ThumbUp")
-        }
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        imageView.frame = CGRectMake(50, 50, 100, 100)
-    }
     
 }
---
+++ EatUp/GGView.swift
@@ -0,0 +1,30 @@
+//
+//  GGView.swift
+//  EatUp
+//
+//  Created by Jeff Ligon on 2/6/15.
+//  Copyright (c) 2015 Visceral Origami LLC. All rights reserved.
+//
+
+import UIKit
+
+class GGView: UIView {
+    var draggableView :DraggableView!
+    override init(){
+        super.init()
+        backgroundColor = UIColor.whiteColor()
+        loadDraggableView()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension GGView{
+    func loadDraggableView(){
+        self.draggableView = DraggableView(frame: CGRectMake(60, 60, 200, 260))
+        addSubview(self.draggableView)
+    }
+}
